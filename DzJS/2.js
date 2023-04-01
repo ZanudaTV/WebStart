61c8e493e99056080bb3ac3efbfeb465b2e8eb2b
Teslya Anton <toxa77725@gmail.com>
2023-04-01 14:00:13 +0300

    1st JS works

---
+++ DzJS/2.js
@@ -0,0 +1,5 @@
+const name = prompt("Представьтесь пожалуйста");
+function greeting(userName) {
+    console.log('Привет товарищ' + ' ' + userName);
+}
+greeting(name);
---
+++ Sem3/main.js
@@ -0,0 +1,82 @@
+// https://disk.yandex.ru/d/6VAN_3gLnscEYg дз
+//
+//     11:33
+// https://learn.javascript.ru/ сайт для учебы
+//
+//     11:37 Ниже до пройти
+// https://learn.javascript.ru/error-handling
+//     https://learn.javascript.ru/async
+//         https://learn.javascript.ru/modules
+//             https://learn.javascript.ru/network
+// теперь можно учить рекакт или вьюjs
+
+// Функция getMaxEvenElement принимает массив с целыми числами, необходимо
+// реализовать функцию так, чтобы она возвращала значение большего элемента массива,
+// который записан в четном индексе включая 0.
+
+function getMaxEvenElement(arr) {
+    let max = arr[0];
+    for (let i = 2; i < arr.length; i += 2) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max;
+}
+
+console.log(getMaxEvenElement([5, 7, -1, 12, 3, 0])); // 5
+console.log(getMaxEvenElement([4, -12, 29, 6, 31, 92, -50])); // 31
+
+
+// Задача 5: перепишите код, используя конструкцию switch-case, запрашивая продукт через диалоговое окно.
+
+// <script>
+//     let product = "Бананы";
+//
+//     if (product == "Мандарины") {
+//     alert('Мандарины стоят 100 руб/кг.');
+// } else if (product == "Бананы") {
+//     alert('Бананы и груши стоят 70 руб/кг.');
+// } else if (product == "Груши") {
+//     alert('Бананы и груши стоят 70 руб/кг.');
+// } else {
+//     alert('Нет такого продукта.');
+// }
+// </script>
+
+// const product = 'Бананы';
+// switch (product) {
+//     case 'Мандарины':
+//         alert('Мандарины стоят 100 руб/кг.');
+//         break;
+//     case 'Бананы':
+//     case 'Груши':
+//         alert('Бананы и груши стоят 70 руб/кг.');
+//         break;
+//     default:
+//         alert('Нет такого продукта.');
+// }
+//
+
+
+// const num1 = Number.parseInt(prompt("Введите первое число:"));
+// const num2 = Number.parseInt(prompt("Введите второе число:"));
+// alert(`Результат сложения чисел ${num1} и ${num2} равен ${num1 + num2}`);
+// function sum(num1, num2) {
+//     return num1 + num2;
+// }
+// const num3 = Number.parseFloat(prompt("Введите первое число:"));
+// const num4 = Number.parseFloat(prompt("Введите второе число:"));
+// alert(`Результат сложения чисел ${num3} и ${num4} равен ${sum(num3, num4).toFixed(2)}`);
+
+// Задача 4: вывести на экран в диалоговом окне текст с сообщением “Вам хорошо живется?”
+// и кнопками «ОК», «Отмена», для чего необходимо использовать confirm.
+// - При нажатии на кнопку “ОК” вывести в диалоговом окне текст с сообщением “Тогда мы идем к вам!”.
+// - При нажатии на кнопку “Отмена” вывести в диалоговом окне текст с сообщением “Ну вы держитесь там!”.
+// const answer = confirm('Вам хорошо живется?');
+// if (answer) {
+//     alert('Тогда мы идем к вам!');
+// } else {
+//     alert('Ну вы держитесь там!');
+// }
+
